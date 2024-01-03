3eb7381356b75fcb57fe9e66723d240d711a0648
Enrico Talotti <enriconh@gmail.com>
2024-01-03 14:27:34 +0100

    Test
    Tests added in sumcheck.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,24 @@
+use ark_bls12_381::Fr;
 use ark_ff::{Fp, MontBackend};
 use ark_poly::{
     polynomial::multivariate::{SparsePolynomial, SparseTerm, Term},
     DenseMVPolynomial};
-use sumcheck::sumcheck::{ScalarField,sum_check,slow_sum_g};
+use sumcheck::sumcheck::{sum_check,slow_sum_g, n_to_vec};
 fn main() {
+    let num:usize=8;
+    let vec=n_to_vec(8, 8);
+    println!("{} to vec is {:?}",num,vec);
     let poly: SparsePolynomial<Fp<MontBackend<ark_bls12_381::FrConfig, 4>, 4>, SparseTerm> = SparsePolynomial::from_coefficients_vec(
         3,
         vec![
-            (ScalarField::from(2), SparseTerm::new(vec![(0, 3)])),
-            (ScalarField::from(1), SparseTerm::new(vec![(0, 1), (2, 1)])),
-            (ScalarField::from(1), SparseTerm::new(vec![(1, 1), (2, 1)])),
+            (Fr::from(2), SparseTerm::new(vec![(0, 3)])),
+            (Fr::from(1), SparseTerm::new(vec![(0, 1), (2, 1)])),
+            (Fr::from(1), SparseTerm::new(vec![(1, 1), (2, 1)])),
         ],
     );
 //println!("poly is {:?}",poly);
 //println!("number of variable of poly is {}",n_var);
 let h=slow_sum_g(&poly);
+println!("h is {}",h);
 sum_check(poly, h);
 }
